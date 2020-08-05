2b5704ef4015cca419ac42b07e1e18b36aa53df1
hendrihmwn <hendrihmwn@gmail.com>
2020-08-05 14:07:03 +0700

    coba lagi

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
 	export let name;
+	export let desc;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>{{desc}}</p>
+	<p>{desc}</p>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
